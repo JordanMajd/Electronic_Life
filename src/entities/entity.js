310eb7724acf2dbf1037d2f222d891f19561ca18
Jordan Majd <jordan.majd@galvanize.com>
2015-12-07 16:29:49 -0700

    removed the entity solution

--- src/entities/entity.js
+++ src/entities/entity.js
@@ -3,15 +3,8 @@
 var Action = require('../game/action');
 
 function Entity() {
-  this.energy = 100;
-  this.direction = 'n';
 }
 
-Entity.prototype.act  = function(view){
-  if (view.look(this.direction) !== " ") {
-    this.direction = view.find(" ") || "s";
-  }
-  return new Action(Action.types.move, this.direction);
-};
+
 
 module.exports = Entity;
