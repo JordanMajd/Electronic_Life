1210638650030b2c98c47a82548bae20f121727f
Jordan Majd <jordan.majd@galvanize.com>
2015-12-08 14:13:53 -0700

    removed commented code

--- src/game/domworldanimator.js
+++ src/game/domworldanimator.js
@@ -10,15 +10,7 @@ DomWorldAnimator.prototype = Object.create(WorldAnimator.prototype);
 DomWorldAnimator.prototype.tick = function() {
   this.world.turn();
 
-  //term codes
-  // var clearTerm = '\x1B[2J';
-  // var textColor = '';//'\x1b[35m';
-  // var bgColor =  '';//'\x1b[46m';
-  // var clearColor = '\x1b[0m';
-
-  //clear term, set fg and bg color, print world, clear color options.
-  // console.log(clearTerm + textColor + bgColor + this.world.toString() + clearColor);
-  document.querySelectorAll('#canvas')[0].innerHTML = '<pre>'+ this.world.toString() + '</pre>';
+  document.querySelectorAll('#dom-canvas')[0].innerHTML = '<pre>'+ this.world.toString() + '</pre>';
 };
 
 module.exports = DomWorldAnimator;
